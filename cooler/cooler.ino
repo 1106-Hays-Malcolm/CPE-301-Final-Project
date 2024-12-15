dfcef6e5d30b77df9a4cc3490df60cbc6ef1c777
NguDerek <158017790+NguDerek@users.noreply.github.com>
2024-12-15 04:17:42 +0000

    Created a printTimeAndDate function and removed serial calls

--- cooler/cooler.ino
+++ cooler/cooler.ino
@@ -539,6 +539,25 @@ void myPinMode(uint8_t pinNumber, uint8_t mode) {
   }
 }
 
+void printTimeAndDate() {
+  DateTime now = rtc.now();
+  int y = now.year();
+  /*Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek() - 1]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();*/
+}
+
 unsigned long previousMillis = 0;
 const long lcdInterval = 5000; 
 
@@ -588,23 +607,6 @@ void setup() {
   // stepper.step(stepsPerRevolution);
 
   // motorStart(254);
-
-  DateTime now = rtc.now();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-
 }
 
 void loop() {
